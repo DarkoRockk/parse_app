045a2d02026440be2ee7e1dce726ae7b1f3d5d2e
Dmitry Shadrin <d.shadrin@fantasyinvest.club>
2024-02-22 19:41:30 +0200

    mvp

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.parse.ParseService;
+import org.example.parse.bean.WebPage;
+import org.example.web_bazaraki.HtmlUnitParseService;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+        log.error("Hello world!");
+        log.warn("Hello world!");
+
+        var app = new App();
+        app.runApp();
+    }
+
+    public void runApp() {
+        ParseService parseService = new HtmlUnitParseService();
+        parseService.init();
+
+        try {
+            URL url = new URL("http://bazaraki.com");
+            WebPage webPage = parseService.parse(url);
+            log.info("Parsed: {}", webPage);
+        } catch (MalformedURLException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
---
+++ src/main/java/org/example/parse/ParseService.java
@@ -0,0 +1,13 @@
+package org.example.parse;
+
+import org.example.parse.bean.WebPage;
+
+import java.net.URL;
+
+public interface ParseService {
+
+    void init();
+
+    WebPage parse(URL url);
+
+}
